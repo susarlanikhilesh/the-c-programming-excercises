0ed2c5741f0c324ba577b17706b8dc36974a8eb4
susarlanikhilesh <nikhilesh1294@gmail.com>
2019-05-17 16:44:55 +0530

    Added note for backspace

--- chapter-one/p1-10.c
+++ chapter-one/p1-10.c
@@ -2,6 +2,9 @@
   Copy input to output, replacing tab with \t and backspace with \b
   and backslash with \\. This makes tabs and backspaces visible in
   an unambiguous way.
+
+  Note : backspace is not recognized as a character as terminal itself
+         handles it. So, we can't print \b replacing backspaces
 */
 #include<stdio.h>
 int main(void)
