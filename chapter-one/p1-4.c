88b634be6807ea6f4abb349ffe0950b75d3fad28
susarlanikhilesh <nikhilesh1294@gmail.com>
2019-05-17 15:18:37 +0530

    Converting Farhenheit to Celcius

---
+++ chapter-one/p1-4.c
@@ -0,0 +1,23 @@
+/*
+  Program to convert Farhenheit to Celcius
+*/
+
+#include<stdio.h>
+
+float CalculateFarheit(float f)
+{
+    return (5.0/9.0) * (f - 32.0);
+}
+
+int main(void)
+{
+    float farh = 0.0, celcius = 0.0;
+    int upper = 300, step = 20;
+    printf("Farhenheit to Celcius\n");
+    while(farh <= upper)
+    {
+        printf("%3.0f%6.1f\n", farh, CalculateFarheit(farh));
+        farh += step;
+    }
+    return 0;
+}
