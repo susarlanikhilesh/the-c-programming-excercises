c9477bfff50b198031ea1305f1328ab58d68166d
susarlanikhilesh <nikhilesh1294@gmail.com>
2019-05-20 10:45:54 +0530

    Todo : To write this program

---
+++ chapter-one/p1-24.c
@@ -0,0 +1,8 @@
+/*
+    Wrtie a program to check a C program for rudimentary syntax errors
+    like unbalanced paranthese, brackets and braces.
+    Don't forget about quotes, both single and double, escape
+    sequences and comments.
+
+    Todo : Write later
+*/
